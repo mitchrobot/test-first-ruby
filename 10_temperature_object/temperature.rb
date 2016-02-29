d7a619bea2540b798f886f7493d33351bd442109
Mitch Conquer <mitch@robotshanghai.com>
2016-02-29 16:40:57 +0800

    Work on temperature.rb

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,8 +1,32 @@
 class Temperature
 
 	attr_accessor :c, :f
-	def initialize
 
+	def initialize(args = {})
+		@f = args[:f]
+		@c = args[:c]
+		get_fahrenheit
+		get_celsius
+	end
+
+	def get_fahrenheit
+		if @f.nil? && !@c.nil?
+			@f = @c * (9/5.0) + 32
+		end
+	end
+
+	def get_celsius
+		if @c.nil? && !@f.nil?
+			@c = (@f - 32) * (5/9.0)
+		end
+	end
+
+	def in_fahrenheit
+		@f
+	end
+
+	def in_celsius
+		@c
 	end
 
 end
